824ac60905dea67ec0aac647248660181a00ad4c
saltyJeff <saltyJeff@users.noreply.github.com>
2020-01-16 14:57:56 -0800

    pymodbus is broken

--- __main__.py
+++ __main__.py
@@ -1,25 +1,76 @@
-from pymodbus.client.sync import ModbusSerialClient
-from time import time
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.exceptions import ModbusIOException
+from time import time_ns, time
+import json
 import config
+from os import path
+
+file = None
 def main():
+    global file
     # configure modbus
-    client = ModbusSerialClient(method='rtu')
-    client.port = config.DEVICE_NAME
+    client = ModbusClient(method='rtu', port=config.DEVICE_NAME, timeout=2)
     client.baudrate = 115200
     client.parity = 'N'
     client.stopbits = 1
     if not client.connect():
-        # raise Exception("can't connect to modbus")
-        pass
-
+        raise Exception("can't connect to modbus")
     # open file
-    file = open(config.OUTPUT_FILE, 'a+')
-    while True:
-        start = time()
-        file.write("hello there general kenobi have you heard the tragedy of darth plageus the wise i thought not i am all the jedi")
-        delta = time() - start
+    file = open(config.OUTPUT_FILE, 'a')
+    if path.getsize(config.OUTPUT_FILE) == 0:
+        print(buildHeader(), file=file)
 
-        print(delta)
+    # cache
+    cache = buildCache()
+    print(cache)
+    lastNotify = time()
+    while True:
+        start = time_ns()
+
+        regResp = client.read_input_registers(config.START_REGISTER,count=len(config.REGISTER_LABELS), unit=15)
+        if isinstance(regResp, ModbusIOException):
+            continue
+        for reg in regResp.registers:
+            file.write(str(reg)+',')
+
+        if time() > lastNotify + config.NOTIFY_WORLD_FREQ:
+            indicies = [0] * len(config.LABELS)
+            for i,reg in enumerate(regResp.registers):
+                labelIdx = config.REGISTER_LABELS[i]
+                if labelIdx is 0:
+                    continue
+                labelIdx = labelIdx - 1
+                label = config.LABELS[labelIdx]
+                cache[label][indicies[labelIdx]] = reg
+                indicies[labelIdx] = indicies[labelIdx] + 1
+            print(cache)
+            lastNotify = time()
+        
+        delta = time_ns() - start
+        print(delta, file=file)
+
+def buildCache():
+    cache = {}
+    for label in config.LABELS:
+        cache[label] = []
+    for token in config.REGISTER_LABELS:
+        if token == 0:
+            continue
+        label = config.LABELS[token - 1]
+        cache[label].append(0)
+    return cache
+def buildHeader():
+    header = ""
+    for token in config.REGISTER_LABELS:
+        if token == 0:
+            header = header+'0,'
+            continue
+        header = header + config.LABELS[token-1] + ','
+    header = header + 'fps'
+    return header
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except:
+        file.close()
--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-DEVICE_NAME = 'abc.xyz'
+DEVICE_NAME = 'COM8'
 OUTPUT_FILE = 'hello.txt'
+
+# Modbus Config
+SLAVE_ID = 15
+LABELS = ['tc', 'loadcell', 'pt']
+START_REGISTER = 1001
+REGISTER_LABELS = [1, 1, 1]
+
+# stdout config
+NOTIFY_WORLD_FREQ = 0.001
